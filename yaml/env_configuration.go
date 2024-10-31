84bd56239c6cc17dddc3c95a6dd98e3c7bdec769
Brad Sickles <brad.sickles@gmail.com>
2024-10-31 16:21:27 -0400

    Fixed nil panic loading env config from workspace config

--- yaml/env_configuration.go
+++ yaml/env_configuration.go
@@ -29,27 +29,53 @@ func ParseEnvConfiguration(data []byte) (*EnvConfiguration, error) {
 }
 
 func EnvConfigurationFromWorkspaceConfig(stackId, envId int64, block types.Block, config types.WorkspaceConfig) EnvConfiguration {
-	result := EnvConfiguration{}
-	result.Version = "0.1"
+	result := EnvConfiguration{Version: "0.1"}
 
 	switch block.Type {
 	case string(types.BlockTypeApplication):
+		if result.Applications == nil {
+			result.Applications = map[string]AppConfiguration{}
+		}
 		result.Applications[block.Name] = AppConfigurationFromWorkspaceConfig(stackId, envId, config)
 	case string(types.BlockTypeDatastore):
+		if result.Datastores == nil {
+			result.Datastores = map[string]DatastoreConfiguration{}
+		}
 		result.Datastores[block.Name] = DatastoreConfiguration{BlockConfiguration: BlockConfigurationFromWorkspaceConfig(stackId, envId, config)}
 	case string(types.BlockTypeSubdomain):
+		if result.Subdomains == nil {
+			result.Subdomains = map[string]SubdomainConfiguration{}
+		}
 		result.Subdomains[block.Name] = SubdomainConfigurationFromWorkspaceConfig(stackId, envId, block, config)
 	case string(types.BlockTypeDomain):
+		if result.Domains == nil {
+			result.Domains = map[string]DomainConfiguration{}
+		}
 		result.Domains[block.Name] = DomainConfigurationFromWorkspaceConfig(stackId, envId, block, config)
 	case string(types.BlockTypeIngress):
+		if result.Ingresses == nil {
+			result.Ingresses = map[string]IngressConfiguration{}
+		}
 		result.Ingresses[block.Name] = IngressConfiguration{BlockConfiguration: BlockConfigurationFromWorkspaceConfig(stackId, envId, config)}
 	case string(types.BlockTypeClusterNamespace):
+		if result.ClusterNamespaces == nil {
+			result.ClusterNamespaces = map[string]ClusterNamespaceConfiguration{}
+		}
 		result.ClusterNamespaces[block.Name] = ClusterNamespaceConfiguration{BlockConfiguration: BlockConfigurationFromWorkspaceConfig(stackId, envId, config)}
 	case string(types.BlockTypeCluster):
+		if result.Clusters == nil {
+			result.Clusters = map[string]ClusterConfiguration{}
+		}
 		result.Clusters[block.Name] = ClusterConfiguration{BlockConfiguration: BlockConfigurationFromWorkspaceConfig(stackId, envId, config)}
 	case string(types.BlockTypeNetwork):
+		if result.Networks == nil {
+			result.Networks = map[string]NetworkConfiguration{}
+		}
 		result.Networks[block.Name] = NetworkConfiguration{BlockConfiguration: BlockConfigurationFromWorkspaceConfig(stackId, envId, config)}
 	default:
+		if result.Blocks == nil {
+			result.Blocks = map[string]BlockConfiguration{}
+		}
 		result.Blocks[block.Name] = BlockConfigurationFromWorkspaceConfig(stackId, envId, config)
 	}
 
